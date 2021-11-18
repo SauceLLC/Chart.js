2085e391ccf4f4ac70518441a7406bb704f27d99
Justin Mayfield <tooker@gmail.com>
2021-11-17 18:12:24 -0700

    Remove unused charts from our build

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var bar = require('./controller.bar');
-var bubble = require('./controller.bubble');
-var doughnut = require('./controller.doughnut');
-var horizontalBar = require('./controller.horizontalBar');
+//var bubble = require('./controller.bubble');
+//var doughnut = require('./controller.doughnut');
+//var horizontalBar = require('./controller.horizontalBar');
 var line = require('./controller.line');
-var polarArea = require('./controller.polarArea');
-var pie = require('./controller.pie');
-var radar = require('./controller.radar');
+//var polarArea = require('./controller.polarArea');
+//var pie = require('./controller.pie');
+//var radar = require('./controller.radar');
 var scatter = require('./controller.scatter');
 
 // NOTE export a map in which the key represents the controller type, not
@@ -16,12 +16,12 @@ var scatter = require('./controller.scatter');
 
 module.exports = {
 	bar: bar,
-	bubble: bubble,
-	doughnut: doughnut,
-	horizontalBar: horizontalBar,
+//	bubble: bubble,
+//	doughnut: doughnut,
+//	horizontalBar: horizontalBar,
 	line: line,
-	polarArea: polarArea,
-	pie: pie,
-	radar: radar,
+//	polarArea: polarArea,
+//	pie: pie,
+//	radar: radar,
 	scatter: scatter
 };
--- src/scales/index.js
+++ src/scales/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var category = require('./scale.category');
+//var category = require('./scale.category');
 var linear = require('./scale.linear');
-var logarithmic = require('./scale.logarithmic');
-var radialLinear = require('./scale.radialLinear');
+//var logarithmic = require('./scale.logarithmic');
+//var radialLinear = require('./scale.radialLinear');
 var time = require('./scale.time');
 
 module.exports = {
-	category: category,
+//	category: category,
 	linear: linear,
-	logarithmic: logarithmic,
-	radialLinear: radialLinear,
+//	logarithmic: logarithmic,
+//	radialLinear: radialLinear,
 	time: time
 };
