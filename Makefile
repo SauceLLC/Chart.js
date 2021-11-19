f32382afdf05c4da98b6310e243f4a99a6f48b65
Justin Mayfield <tooker@gmail.com>
2021-11-18 19:09:38 -0700

    Get rid of cleanup  plugin (redundant with terser)

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-all: src
+
+all: node_modules/.build
 	./node_modules/.bin/rollup -c
+	@echo -e "\n *** Build Done ***\n"
+	@du -h dist/*.js
+
+node_modules/.build: package.json
+	npm install
+	touch $@
+
+distclean:
+	rm -rf node_modules
+	rm -rf dist/*
+
+clean:
+	rm -rf dist/*
+
+.PHONY: clean distclean all
--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ const plugins = require('./rollup.plugins');
 const commonjs = require('@rollup/plugin-commonjs');
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
 const {terser} = require('rollup-plugin-terser');
-const cleanup = require('rollup-plugin-cleanup');
 
 const stylesheet = plugins.stylesheet;
 
@@ -17,73 +16,62 @@ const banner = `/*!
  * Released under the MIT License
  */`;
 
-module.exports = [
-	// dist/Chart.min.js
-	// dist/Chart.js
-	{
-		input: input,
-		plugins: [
-			nodeResolve(),
-			commonjs(),
-			stylesheet({
-				extract: true
-			}),
-            cleanup({
-                comments: 'none',
-                lineEndings: 'unix',
-                maxEmptyLines: 1,
-                sourcemap: false,
-            }),
-			terser({
-                ecma: 6,
-                keep_classnames: true,
-                keep_fnames: true,
-                compress: {
-                    defaults: false,
-                    arrows: true,
-                    dead_code: true,
-                    unused: true,
-                    evaluate: true,
-                },
-                format: {
-                    beautify: true,
-                    indent_level: 4,
-                    comments: false,
-                    keep_numbers: true,
-                    quote_style: 3, // use orig
-					preamble: banner,
-                    braces: true,
-                }
-            })
-		],
-		output: {
-			name: 'Chart',
-			file: 'dist/Chart.js',
-			format: 'umd',
-			indent: false,
-		}
-	},
-	{
-		input: input,
-		plugins: [
-			nodeResolve(),
-			commonjs(),
-			stylesheet({
-				extract: true,
-				minify: true
-			}),
-			terser({
-                ecma: 6,
-				format: {
-					preamble: banner
-				}
-			})
-		],
-		output: {
-			name: 'Chart',
-			file: 'dist/Chart.min.js',
-			format: 'umd',
-			indent: false,
-		},
-	}
-];
+module.exports = [{
+    input,
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        stylesheet({
+            extract: true
+        }),
+        terser({
+            ecma: 6,
+            keep_classnames: true,
+            keep_fnames: true,
+            compress: {
+                defaults: false,
+                arrows: true,
+                dead_code: true,
+                unused: true,
+                evaluate: true,
+            },
+            format: {
+                beautify: true,
+                indent_level: 4,
+                comments: false,
+                keep_numbers: true,
+                quote_style: 3, // use orig
+                preamble: banner,
+                braces: true,
+            }
+        })
+    ],
+    output: {
+        name: 'Chart',
+        file: 'dist/Chart.js',
+        format: 'umd',
+        indent: false,
+    }
+}, {
+    input,
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        stylesheet({
+            extract: true,
+            minify: true
+        }),
+        terser({
+            ecma: 6,
+            format: {
+                preamble: banner
+            }
+        })
+    ],
+    output: {
+        name: 'Chart',
+        file: 'dist/Chart.min.js',
+        format: 'umd',
+        indent: false,
+    },
+}];
